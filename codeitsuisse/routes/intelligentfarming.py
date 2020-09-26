3ef2374ef9c3f486a5b74ad5e65817004147b958
chris <chonghern@gmail.com>
2020-09-26 15:42:54 +0800

    max acgt only if excess fillers, otherwise max cc

--- codeitsuisse/routes/intelligentfarming.py
+++ codeitsuisse/routes/intelligentfarming.py
@@ -24,13 +24,28 @@ def farming():
     sorted_seq = []
     logging.info("Start seq :{}".format(str(genes)))
     fillers_needed = math.ceil(genes["a"] / 2) - 1
-    # always maximise cgt
+    # always maximise cgt as long as it does not produce aaa sequences
     cgt = min([genes["c"], genes["g"], genes["t"]])
     genes["c"] -= cgt
     genes["g"] -= cgt
     genes["t"] -= cgt
+    num_fillers = genes["c"] + genes["g"] + genes["t"] + cgt
+    logging.info("fillers present: {}".format(num_fillers))
+    fillers_needed = math.ceil(genes["a"]/2) - 1
+    logging.info("fillers needed: {}".format(fillers_needed))
+    excess_fillers = num_fillers - fillers_needed
+    logging.info("excess fillers: {}".format(excess_fillers))
+    if excess_fillers <= 0:
+        excess = math.ceil(abs(excess_fillers)/2)
+        excess = min([excess, cgt])
+        logging.info("excess cgt: {}".format(excess))
+        cgt -= excess
+        genes["c"] += excess
+        genes["g"] += excess
+        genes["t"] += excess
     for i in range(cgt):
         sorted_seq.append("cgt")
+    # maximise cc sequences next
     cc = math.floor(genes["c"]/2)
     excess_c = genes["c"] % 2
     for i in range(cc):
