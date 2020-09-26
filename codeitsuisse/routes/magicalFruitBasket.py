2429ad752cb511a8a28d5f825641c6e88bd2be26
Emily Haw <haw.emily@gmail.com>
2020-09-26 17:47:56 +0800

    remove none type error

--- codeitsuisse/routes/magicalFruitBasket.py
+++ codeitsuisse/routes/magicalFruitBasket.py
@@ -13,10 +13,11 @@ def evaluate_fruit_basket():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     fruits = []
-    if data == {}:
+    if data is None:
         return json.dumps("0")
     for (k, v) in data.items():
         fruits.append(v)
+    logging.info("fruits " + str(fruits))
     result = guessFruitWeight(fruits)
     logging.info("My result :{}".format(result))
     return json.dumps(result)
