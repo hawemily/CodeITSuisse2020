2c3ce7d87542a8bacc1f1fc5070740878837b159
chris <chonghern@gmail.com>
2020-09-26 22:29:36 +0800

    social distancing

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -6,5 +6,6 @@ import codeitsuisse.routes.saladspree
 import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.babylonOlympiad
 import codeitsuisse.routes.intelligentfarming
+import codeitsuisse.routes.socialdistancing
 
 
