190d74d5f44d79af68252e02e235cb58f1be2553
chris <chonghern@gmail.com>
2020-09-26 12:55:35 +0800

    revisit geometry

---
+++ codeitsuisse/routes/revisitgeometry.py
@@ -0,0 +1,58 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        result = a[0] * b[1] - a[1] * b[0]
+        return result
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return
+        
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return round(x, 2), round(y, 2)
+
+@app.route('/revisitgeometry', methods=['POST'])
+def geometry():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    shapeCoordinates = data.get("shapeCoordinates")
+    lineCoordinates = data.get("lineCoordinates")
+    solutions = []
+    for i in range(len(shapeCoordinates) - 1):
+        x, y = line_intersection(
+            [[shapeCoordinates[i]["x"], shapeCoordinates[i]["y"]], # start point
+            [shapeCoordinates[i+1]["x"], shapeCoordinates[i+1]["y"]]] # end point
+            ,
+            [[lineCoordinates[0]["x"], lineCoordinates[0]["y"]],  # start point
+            [lineCoordinates[1]["x"], lineCoordinates[1]["y"]]])
+        if x != None and y != None:
+            logging.info("Intersection found between line and edge {}".format(i))
+            solutions.append({"x": x, "y": y})
+    
+    i = len(shapeCoordinates) - 1
+    x, y = line_intersection(
+        [[shapeCoordinates[i]["x"], shapeCoordinates[i]["y"]], # start point
+        [shapeCoordinates[0]["x"], shapeCoordinates[0]["y"]]] # end point
+        ,
+        [[lineCoordinates[0]["x"], lineCoordinates[0]["y"]],  # start point
+        [lineCoordinates[1]["x"], lineCoordinates[1]["y"]]])
+    if x != None and y != None:
+        logging.info("Intersection found between line and edge {}".format(i))
+        solutions.append({"x": x, "y": y}) 
+
+    logging.info("My result :{}".format(solutions))
+    return json.dumps(solutions)
