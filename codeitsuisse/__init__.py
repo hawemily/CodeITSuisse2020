1474b02083360da7f2a7db53f7609ccaaebe58aa
chris <chonghern@gmail.com>
2020-09-26 22:32:15 +0800

    merge

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -6,10 +6,7 @@ import codeitsuisse.routes.saladspree
 import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.babylonOlympiad
 import codeitsuisse.routes.intelligentfarming
-<<<<<<< HEAD
 import codeitsuisse.routes.socialdistancing
-=======
 import codeitsuisse.routes.magicalFruitBasket
->>>>>>> e767f8cfd264a6ba48d7846ffc63c93e23a6a424
 
 
