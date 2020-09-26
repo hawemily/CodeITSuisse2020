20a39adf628a398c29efd16eaf819a0454e5f8cb
Emily Haw <haw.emily@gmail.com>
2020-09-26 23:40:44 +0800

    optimised portfolio code

---
+++ codeitsuisse/routes/optimisedPortfolio.py
@@ -0,0 +1,47 @@
+import logging
+import json
+import sys
+
+from flask import request, jsonify;
+from decimal import Decimal
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/optimizedportfolio', methods=['POST'])
+def evaluate_portfolio():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inputs = data["inputs"]
+
+    ls = []
+    for input in inputs:
+        portfolio = input["Portfolio"]
+        (lowestFutName, lowestHR, lowestFutures) = calculateLowestHedge(input["IndexFutures"], portfolio["Value"],
+                                                                        portfolio["SpotPrcVol"])
+        ls.append(
+            {"HedgePositionName": lowestFutName, "OptimalHedgeRatio": lowestHR, "NumFuturesContract": lowestFutures})
+
+    outputs = {"outputs": ls}
+
+    logging.info("My result :{}".format(outputs))
+    return json.dumps(outputs)
+
+
+def calculateLowestHedge(indexFutures, value, spotPrcSD):
+    lowestFutures = sys.maxsize
+    lowestHR = 0
+    lowestFut = ""
+    for indexFuture in indexFutures:
+        hedgeRatio = Decimal(str(indexFuture["CoRelationCoefficient"] * spotPrcSD / indexFuture["FuturePrcVol"]))
+        roundedHedgeRatio = round(hedgeRatio, 3)
+        futuresContract = Decimal(
+            str(roundedHedgeRatio * value / (indexFuture["IndexFuturePrice"] * indexFuture["Notional"])))
+        roundFutureContract = int(round(futuresContract))
+        if lowestFutures > roundFutureContract:
+            lowestFutures = roundFutureContract
+            lowestHR = roundedHedgeRatio
+            lowestFut = indexFuture["Name"]
+    return lowestFut, lowestHR, lowestFutures
